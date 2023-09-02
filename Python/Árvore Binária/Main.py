5203883b9004291d946e3b44c897ead7eb13bd82
Guilherme Esteves <guilherme.o.esteves@gmail.com>
2023-09-02 11:35:42 -0300

    Opções para Usuário - Main

--- Python/Árvore Binária/Main.py
+++ Python/Árvore Binária/Main.py
@@ -3,24 +3,44 @@ from os import system
 
 system('cls')
 
+opts = """1. Inserir Valor na árvore
+2. Remover Valor da árvore 
+3. Exibição InOrder
+4. Exibição PreOrder
+5. Exibição PosOrder
+\n0. Finalizar Programa\n"""
+
 ArvoreBinaria = arvore_arq.ArvoreBinaria()
 
-ArvoreBinaria.Insert(15)
-ArvoreBinaria.Insert(10)
-ArvoreBinaria.Insert(17)
-ArvoreBinaria.Insert(16)
-ArvoreBinaria.Insert(8)
-ArvoreBinaria.Insert(23)
-ArvoreBinaria.Insert(12)
-ArvoreBinaria.Insert(14)
+while True:
+    print(opts)
 
-ArvoreBinaria.InOrder()
-ArvoreBinaria.PreOrder()
-ArvoreBinaria.PosOrder()
+    opt = int(input("Digite a opção que deseja: "))
+    system("cls")
 
+    if opt == 1:
+        print("1. Inserir Valor na árvore\n")
 
-ArvoreBinaria.remove(17)
+        value = int(input("Digite o valor que deseja inserir: "))
+        ArvoreBinaria.Insert(value)
+        system("cls")
 
-ArvoreBinaria.InOrder()
-ArvoreBinaria.PreOrder()
-ArvoreBinaria.PosOrder()
+    elif opt == 2:
+        print("2. Remover Valor da árvore\n")
+
+        value = int(input("Digite o valor que deseja remover: "))
+        print(f"O valor foi removido da árvore.")
+
+    elif opt == 3:
+        ArvoreBinaria.InOrder()
+
+    elif opt == 4:
+        ArvoreBinaria.PreOrder()
+
+    elif opt == 5:
+        ArvoreBinaria.PosOrder()
+
+    elif opt == 0:
+        break
+
+print("Programa Finalizado")
